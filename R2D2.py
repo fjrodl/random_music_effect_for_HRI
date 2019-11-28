c8d399d44b51a5ea058b21d44f00fe1c27e761c5
test <you@example.com>
2019-11-28 19:00:28 +0100

    This is a random example for generating random sounds using python

---
+++ R2D2.py
@@ -0,0 +1,96 @@
+# Copyright (c) 2019 fjrodl
+# All rights reserved.
+
+# Redistribution and use in source and binary forms are permitted
+# provided that the above copyright notice and this paragraph are
+# duplicated in all such forms and that any documentation,
+# advertising materials, and other materials related to such
+# distribution and use acknowledge that the software was developed
+# by the <organization>. The name of the
+# <organization> may not be used to endorse or promote products derived
+# from this software without specific prior written permission.
+# THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+
+import cv2
+import sys
+import logging as log
+import datetime as dt
+from time import sleep
+import math        #import needed modules
+import pyaudio     #sudo apt-get install python-pyaudio
+import random
+
+import numpy as np
+
+p = pyaudio.PyAudio()
+
+cascPath = "config/haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascPath)
+log.basicConfig(filename='log/info.log',level=log.INFO)
+
+video_capture = cv2.VideoCapture(0)
+anterior = 0
+
+
+def musiclayer(value1, value2):
+    volume = 0.5     # range [0.0, 1.0]
+    fs = 44100       # sampling rate, Hz, must be integer
+    duration = 0.1*random.randint(1,10)   # in seconds, may be float
+    f = value2*random.randint(1,10)        # sine frequency, Hz, may be float
+    # generate samples, note conversion to float32 array
+    samples = (np.sin(2*np.pi*np.arange(fs*duration)*f/fs)).astype(np.float32)
+
+    # for paFloat32 sample values must be in range [-1.0, 1.0]
+    stream = p.open(format=pyaudio.paFloat32,
+                    channels=1,
+                    rate=fs,
+                    output=True)
+
+    # play. May repeat with different volume values (if done interactively) 
+    stream.write(volume*samples)
+
+    stream.stop_stream()
+    stream.close()
+
+while True:
+    if not video_capture.isOpened():
+        print('Unable to load camera.')
+        sleep(5)
+        pass
+
+    # Capture frame-by-frame
+    ret, frame = video_capture.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30)
+    )
+
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        log.info("Rectangle size: X "+ str(x) +" Y "+ str(y) )
+        musiclayer(x,y)
+
+    if anterior != len(faces):
+        anterior = len(faces)
+        log.info("faces: "+str(len(faces))+" at "+str(dt.datetime.now()))
+
+
+    if(False):
+        # Display the resulting frame
+        cv2.imshow('Video', frame)
+
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        
+# When everything is done, release the capture
+video_capture.release()
+cv2.destroyAllWindows()
